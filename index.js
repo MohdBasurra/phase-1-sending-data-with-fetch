314bc24661c450305ea2e0c575e8f8c41a216a4f
MohdBasurra <mohdye96@gmail.com>
2023-03-18 14:00:07 +0300

    assigment comleted

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData( name, email ) {
+    const url = 'http://localhost:3000/users';
+    const data = { name: name, email: email };
+    const config = {
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+
+    //return the fetch 
+    return fetch(url, config)
+        .then(response => response.json())
+        .then(data => {
+            const id = data.id;
+            document.body.innerHTML += id
+        })
+        .catch(error => {
+            document.body.innerHTML += error.message
+        });
+}
